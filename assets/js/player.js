fa78d85620bc9cf2119c3ead1605baa91f893dd6
Francesco Masala <mail@francescomasala.me>
2022-01-31 10:56:11 +0100

    Added Javascript

---
+++ assets/js/player.js
@@ -0,0 +1,181 @@
+let now_playing = document.getElementById("podcastnav");
+let track_name = document.querySelector(".track-name");
+let track_artist = document.querySelector(".track-artist");
+
+let playpause_btn = document.querySelector(".playpause-track");
+let next_btn = document.querySelector(".next-track");
+let prev_btn = document.querySelector(".prev-track");
+
+let seek_slider = document.querySelector(".seek_slider");
+let volume_slider = document.querySelector(".volume_slider");
+let curr_time = document.querySelector(".current-time");
+let total_duration = document.querySelector(".total-duration");
+
+let currentSeconds;
+let currentMinutes;
+
+let somethingPlay = false;
+
+let track_index = 0;
+let isPlaying = false;
+let updateTimer;
+
+//FullScren
+function fullScreen() {
+    let eta = Math.floor((currentMinutes * 60) + currentSeconds);
+    window.location.href = "fullscreen.html?episode=" + track_index;
+}
+
+function checkMobile() {
+    let check = false;
+    (function(a) {
+        if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
+    })(navigator.userAgent || navigator.vendor || window.opera);
+    return check;
+};
+
+function playFullScreen() {
+
+    let urlParams = new URLSearchParams(window.location.search);
+    let episode = urlParams.get('episode');
+    loadTrack(episode);
+    playTrack();
+}
+
+
+// Create new audio element
+let curr_track = document.createElement('audio');
+
+// Define the tracks that have to be played
+let track_list = [{
+        name: "Innovazione industriale",
+        desc: "",
+        active: true,
+        path: "assets\podcasts\first.mp3"
+    },
+];
+
+function playNewTrack(index) {
+    if (checkMobile() == true) {
+        fullScreen();
+    } else {
+        loadTrack(index);
+        playTrack();
+    }
+}
+
+function loadTrack(track_index) {
+    clearInterval(updateTimer);
+    resetValues();
+
+    // Load a new track
+    curr_track.src = track_list[track_index].path;
+    curr_track.load();
+
+    // Update details of the track
+    document.title = track_list[track_index].name + " (OpenVoice Streaming)";
+    track_name.textContent = track_list[track_index].name;
+    now_playing.textContent = "Puntata " + (track_index + 1) + " di " + track_list.length;
+
+    // Set an interval of 1000 milliseconds for updating the seek slider
+    updateTimer = setInterval(seekUpdate, 100);
+
+    // Move to the next track if the current one finishes playing
+    curr_track.addEventListener("ended", nextTrack);
+}
+
+// Reset Values
+function resetValues() {
+    curr_time.textContent = "00:00";
+    total_duration.textContent = "00:00";
+    seek_slider.value = 0;
+}
+
+function playpauseTrack() {
+    if (!isPlaying) playTrack();
+    else pauseTrack();
+}
+
+function showPlayer() {
+    let element = document.getElementById("player");
+    element.classList.remove("invisible");
+}
+
+
+function playTrack() {
+    curr_track.play();
+    isPlaying = true;
+    if (somethingPlay == false) {
+        somethingPlay = true;
+        showPlayer();
+    }
+
+    // Replace icon with the pause icon
+    playpause_btn.innerHTML = '<i class="fa fa-pause"></i>';
+}
+
+function pauseTrack() {
+    curr_track.pause();
+    isPlaying = false;
+
+    // Replace icon with the play icon
+    playpause_btn.innerHTML = '<i class="fa fa-play"></i>';;
+}
+
+function nextTrack() {
+    if (track_index < track_list.length - 1)
+        track_index += 1;
+    else track_index = 0;
+    loadTrack(track_index);
+    playTrack();
+}
+
+function prevTrack() {
+    if (track_index > 0)
+        track_index -= 1;
+    else track_index = track_list.length;
+    loadTrack(track_index);
+    playTrack();
+}
+
+function seekTo() {
+    seekto = curr_track.duration * (seek_slider.value / 100);
+    curr_track.currentTime = seekto;
+}
+
+function setVolume() {
+    curr_track.volume = volume_slider.value / 100;
+}
+
+function seekUpdate() {
+    let seekPosition = 0;
+
+    // Check if the current track duration is a legible number
+    if (!isNaN(curr_track.duration)) {
+        seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+        seek_slider.value = seekPosition;
+
+        // Calculate the time left and the total duration
+        currentMinutes = Math.floor(curr_track.currentTime / 60);
+        currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+        let durationMinutes = Math.floor(curr_track.duration / 60);
+        let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+
+        // Adding a zero to the single digit time values
+        if (currentSeconds < 10) {
+            currentSeconds = "0" + currentSeconds;
+        }
+        if (durationSeconds < 10) {
+            durationSeconds = "0" + durationSeconds;
+        }
+        if (currentMinutes < 10) {
+            currentMinutes = "0" + currentMinutes;
+        }
+        if (durationMinutes < 10) {
+            durationMinutes = "0" + durationMinutes;
+        }
+
+        curr_time.textContent = currentMinutes + ":" + currentSeconds;
+        total_duration.textContent = durationMinutes + ":" + durationSeconds;
+    }
+}
